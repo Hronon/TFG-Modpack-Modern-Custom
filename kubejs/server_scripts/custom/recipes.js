b2a0f3ec43d16113984e9fd359433b0473b01a9a
hronon <hronontm@yandex.ru>
2026-07-13 13:10:05 +0300

    create mobile packages

--- kubejs/server_scripts/custom/recipes.js
+++ kubejs/server_scripts/custom/recipes.js
@@ -201,4 +201,35 @@ const registerCustomRecipes = (event) =>
 		D: 'minecraft:polished_deepslate',
 		I: '#gtceu:circuits/lv',
 	}).id('tfg:shaped/simpleradio/radiosmither')
+
+	// Create: Mobile Packages
+
+	//# Robo bee
+	event.remove({ output: 'create_mobile_packages:bee_port' })
+	event.shaped('create_mobile_packages:bee_port', [
+		'ATA',
+		'CPC',
+		'CCC',
+	], {
+		C: 'minecraft:andesite_casing',
+		T: 'create:transmitter',
+		P: 'create:packager',
+		A: 'create:andesite_alloy',
+	}).id('tfg:shaped/create_mobile_packages/bee_port')
+
+	//# Bee port
+	event.remove({ output: 'create_mobile_packages:robo_bee' })
+	event.shaped('create_mobile_packages:robo_bee', [
+		'RTR',
+		'ALP',
+		'CVC',
+	], {
+		R: '#forge:rotors',
+		C: 'minecraft:andesite_casing',
+		T: 'create:transmitter',
+		L: '#gtceu:circuits/lv',
+		P: 'create:packager',
+		A: 'create:stock_ticker',
+		V: 'create:item_vault',
+	}).id('tfg:shaped/create_mobile_packages/robo_bee')
 }
